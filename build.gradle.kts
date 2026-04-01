53aaacb7d496c464300a8019090a9ad7e65506fc
jhanva <jarbey.mejia@gmail.com>
2026-03-31 22:34:28 -0500

    update tooling and ignore generated artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
+        classpath("com.android.tools.build:gradle:8.13.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.52")
         classpath("com.google.devtools.ksp:symbol-processing-gradle-plugin:1.9.24-1.0.20")
